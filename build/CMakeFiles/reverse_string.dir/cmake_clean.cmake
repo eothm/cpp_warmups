1ef457f7575212e06626597c7f735eb4dc31982a
eothmane <eyaothmane97@gmail.com>
2025-04-15 12:55:43 +0100

    First commit

---
+++ build/CMakeFiles/reverse_string.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverse_string.dir/reverse_string.cpp.o"
+  "CMakeFiles/reverse_string.dir/reverse_string.cpp.o.d"
+  "reverse_string"
+  "reverse_string.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reverse_string.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
