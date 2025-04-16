b6581bda48d0738a1826031d11f3fe24c576ab70
eothmane <eyaothmane97@gmail.com>
2025-04-16 11:52:53 +0100

    refrech cin cout

---
+++ in-out-test/build/CMakeFiles/in-out-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/in-out-test.dir/main.cpp.o"
+  "CMakeFiles/in-out-test.dir/main.cpp.o.d"
+  "in-out-test"
+  "in-out-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/in-out-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ in-out-test/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(){
+int x;
+char y;
+float a;
+std::string b;
+std::cout<< "an int, a char, a float, and a string \n"; 
+std::cin>>x;
+std::cin>>y;
+std::cin>>a;
+std::cin>>b;
+std::cout<<x<<" "<<y<<" "<<a<<" "<<b;
+return 0;
+}
+
+
