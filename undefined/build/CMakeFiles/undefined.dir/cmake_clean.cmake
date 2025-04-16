a6894caec1627431a72b26f58ae9ad472cb44240
eothmane <eyaothmane97@gmail.com>
2025-04-16 12:09:22 +0100

    checked undefined values

---
+++ undefined/build/CMakeFiles/undefined.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undefined.dir/main.cpp.o"
+  "CMakeFiles/undefined.dir/main.cpp.o.d"
+  "undefined"
+  "undefined.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undefined.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ undefined/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int x;
+    float y;
+    std::string b;
+    char c;
+    std::cout<<"empty int "<<x<<"\nempty float "<<y<<"\nempty string "<<b<<"\n empty char "<<c;
+    return 0;
+}
